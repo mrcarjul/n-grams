bf6d151b1369244eca7f453de9d214051a856494
Carlos <mrcarjul@github.com>
2021-11-03 00:55:10 -0600

    created tests for utils #1

---
+++ src/utils/utils.test.js
@@ -0,0 +1,40 @@
+import { cleanPhrase, getNgrams } from "./index";
+
+const phrases = ["Show me the code.", "I am just testing my function"];
+
+describe("getNgrams function validates params received", () => {
+  test("throws error if no text received", () => {
+    expect(() => {
+      getNgrams();
+    }).toThrow("text is required");
+  });
+
+  test("throws error if text is not a string", () => {
+    expect(() => {
+      getNgrams(4);
+    }).toThrow("text must be a string");
+  });
+
+  test("throws error if text is an empty string", () => {
+    expect(() => {
+      getNgrams("");
+    }).toThrow("text must contain at least one character");
+  });
+});
+
+describe("Evaluate getNgrams function phrase with testing phrases", () => {
+  const getPhrasePermutationLength = (phrase) => {
+    const words = cleanPhrase(phrase);
+    return (words.length * (words.length + 1)) / 2;
+  };
+
+  test("should return correct length array for phrases[0]", () => {
+    const ngrams = getNgrams(phrases[0]);
+    expect(ngrams.length).toEqual(getPhrasePermutationLength(phrases[0]));
+  });
+
+  test("should return correct length for phrases[1]", () => {
+    const ngrams = getNgrams(phrases[1]);
+    expect(ngrams.length).toEqual(getPhrasePermutationLength(phrases[1]));
+  });
+});
