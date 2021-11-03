08cd9f865756532b67a87dac4fcc7a7fab5ea3ae
Carlos <mrcarjul@github.com>
2021-11-03 02:14:50 -0600

    correction to test #2

--- src/screens/HomeScreen.test.tsx
+++ src/screens/HomeScreen.test.tsx
@@ -1,20 +1,26 @@
 import React from "react";
-
-// Note: testing-library must be required after react-native.
-import { fireEvent, render } from "@testing-library/react-native";
+import { fireEvent, render, RenderAPI } from "@testing-library/react-native";
 import { HomeScreen } from ".";
 
 describe("HomeScreen UI has correct elements", () => {
-  const { queryByTestId } = render(<HomeScreen />);
+  let componentTree: RenderAPI;
+
+  beforeEach(() => {
+    componentTree = render(<HomeScreen />);
+  });
+
   test("should have a textinput for phrase", () => {
+    const { queryByTestId } = componentTree;
     expect(queryByTestId("inputHome")).not.toBeNull();
   });
 
   test("should have a flatlist to render n-grams permutations", () => {
+    const { queryByTestId } = componentTree;
     expect(queryByTestId("flatlistHome")).not.toBeNull();
   });
 
   test("should have a button to request for n-grams permutations", () => {
+    const { queryByTestId } = componentTree;
     expect(queryByTestId("btnHome")).not.toBeNull();
   });
 });
